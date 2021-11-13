d914725a8aa3ed4dc65f450fe664bb68156d8c35
Gil Tshale <tashala159@gmail.com>
2021-11-13 19:57:37 +0200

    first assingment 2021

--- src/AbstractValue.java
+++ src/AbstractValue.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class AbstractValue {
     public static void main(String[] arg) {
         System.out.println("Please Enter Number ");
--- src/PrintByOrder.java
+++ src/PrintByOrder.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class PrintByOrder {
     public static void main(String[] args) {
         System.out.println("Please enter 3 numbers");
